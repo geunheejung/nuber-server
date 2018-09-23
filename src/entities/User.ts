74556d14c0962a376c62fdc1b6e66490a5e20bba
정근희 <jeong-geunhui@jeong-geunhuiui-MacBook-Pro.local>
2018-09-23 22:59:43 +0900

    password 해싱하여서 저장하도록 변경

---
+++ src/entities/User.ts
@@ -0,0 +1,79 @@
+import bcrypt from 'bcrypt';
+import { IsEmail } from "class-validator";
+import {
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+const BCRYPT_ROUNDS = 10;
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ type: "text", unique: true })
+  @IsEmail()
+  email: string;
+  @Column({ type: "boolean", default: false })
+  verifiedEmail: boolean;
+  @Column({ type: "text" })
+  firstName: string;
+  @Column({ type: "text" })
+  lastName: string;
+  @Column({ type: "int" })
+  age: number;
+  @Column({ type: "text" })
+  password: string;
+  @Column({ type: "text" })
+  phoneNumber: string;
+  @Column({ type: "boolean", default: false })
+  verifiedPhonenNumber: boolean;
+  @Column({ type: "text" })
+  profilePhoto: string;
+
+  @Column({ type: "boolean", default: false })
+  isDriving: boolean;
+  @Column({ type: "boolean", default: false })
+  isRiding: boolean;
+  @Column({ type: "boolean", default: false })
+  isTaken: boolean;
+  @Column({ type: "double precision", default: 0 })
+  lastLng: number;
+  @Column({ type: "double precision", default: 0 })
+  lastLat: number;
+  @Column({ type: "double precision", default: 0 })
+  lastOrientation: number;
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  // 새로운 object를 만들기 전에 불려지는 메소드
+  @BeforeInsert()
+  @BeforeUpdate()
+  // async로 감싼 이유는 hashing 과정이 비동기적으로 이루어지기 때문이다.
+  // 그러므로 hashedPassword 를 얻기 위해서는 hasing이 끝날때까지 await 기다려줘야 한다.
+  async savePassword() : Promise<void> {
+    if (this.password) {
+      // hash한다 즉 암호화한다.
+      const hashedPassword = await this.hashPassword(this.password); 
+      this.password = hashedPassword;
+    }
+  }
+  // object를 업데이트하기 전에
+  private hashPassword(password: string) : Promise<string> {
+    return bcrypt.hash(
+      password,
+      BCRYPT_ROUNDS
+    )
+  }
+} 
+
+export default User;
--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -3,12 +3,11 @@ import { ConnectionOptions } from 'typeorm';
 const ConnectionOptions: ConnectionOptions = {
   type: 'postgres',
   database: 'nuber',
+  // 서버가 재시작될때마다 동기화
   synchronize: true,
   logging: true,
   //  model이 들어갈 folder
-  entities: [
-    'entities/**/*.'
-  ],
+  entities: ['entities/**/*.'],
   host: process.env.DB_ENDPOINT,
   port: 5432,
   username: process.env.DB_USERNAME,
