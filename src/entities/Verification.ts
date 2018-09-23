2839bb82462640884435de9acce983d453c01061
정근희 <jeong-geunhui@jeong-geunhuiui-MacBook-Pro.local>
2018-09-23 23:49:18 +0900

    Verification 엔티티의 target 속성에 type define 및 enum 강제 VerificationTarget

---
+++ src/entities/Verification.ts
@@ -0,0 +1,43 @@
+import { 
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,  
+} from 'typeorm';
+import { VerificationTarget } from '../types/types';
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number
+
+  @Column({ type: 'text', enum: ['PHONE', 'EMAIL'] })  
+  target: VerificationTarget;
+
+  @Column({ type: 'text' })
+  payload: string;
+
+  @Column({ type: 'text' })
+  key: string;
+
+  @Column({ type: 'boolean', default: false })
+  used: boolean;
+
+  @CreateDateColumn() createdAt: string;
+}
+
+export default Verification;
+
+/*
+email이나 phoneNumber를 verify할 때, 
+verification 정보를 db에 만들기 위해서 생성된 파일이다.
+사용자의 email, phoneNumber로 verification정보를 만들고,
+key 값 또한 제작하게 될것
+
+그가 verified되었는지 확인 가능
+
+v.target = phone
+v.payload = +8253852003
+v.key = 3456
+target - email 이였다면 key가 더 길었을 것
+*/
