fef16e59291cf62676999427ae6358c7704e340f
정근희 <jeong-geunhui@jeong-geunhuiui-MacBook-Pro.local>
2018-09-24 22:09:43 +0900

    jwt 용 custom middleware 생성

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,17 @@ class App {
     this.app.express.use(logger('dev'));
     this.app.express.use(helmet());
   };
+
+  /**
+   * request: 무엇을 갖고있는지 확인
+   * request에 따른 response가 있다면 response를 전송
+   * request가 아무것도 갖고있지 않으면 넘어가서 next! 다음 middleware를 실행
+   */
+  private jwt = async (request, response, next): Promise<void> => {
+    const token = request.get('X-JWT');
+    if (token) {
+    }
+  };
 }
 
 export default new App().app;
