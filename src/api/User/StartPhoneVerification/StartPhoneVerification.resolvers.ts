38a27b0046b594917676c6bdb9c1ff3cb0fb4d16
정근희 <jeong-geunhui@jeong-geunhuiui-MacBook-Pro.local>
2018-09-24 19:50:51 +0900

    twilioClien를 통해 sendSMS 테스트 완료

--- src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
+++ src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -4,6 +4,7 @@ import {
   StartPhoneVerificationResponse,
 } from '../../../types/graph';
 import Verification from '../../../entities/Verification';
+import { sendVerificationSMS } from '../../../utils/sendSMS';
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -26,8 +27,14 @@ const resolvers: Resolvers = {
           payload: phoneNumber,
           target: 'PHONE',
         }).save();
-
+        console.log('newVerification :', newVerification);
         // todo: send sms
+        await sendVerificationSMS(newVerification.payload, newVerification.key);
+        return {
+          ok: true,
+          error: null,
+          token: 'Coming Soon',
+        };
       } catch (error) {
         return {
           ok: false,
